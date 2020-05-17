14dce716f1620e5ccc432e7e3aa05199cf5addf6
vish <vishwasvadnerkar@gmail.com>
2020-05-17 12:09:35 +0100

    Add project files.

---
+++ PMS.API/Entities/Project.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PMS.API.Entities
+{
+    public class Project
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime FinishDate { get; set; }
+        public enum State { get; set; }
+    }
+
+    public enum StateType
+    {
+        Planned,
+        InProgress,
+        Completed
+    }
+}
